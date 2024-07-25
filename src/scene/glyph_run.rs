27714d3d5a65a9c1967a14985913ac7aa23c6405
Fred Sundvik <fsundvik@gmail.com>
2024-07-25 12:10:24 +0300

    Add font synthesis settings to GlyphRun

--- src/scene/glyph_run.rs
+++ src/scene/glyph_run.rs
@@ -2,22 +2,70 @@ use std::{fmt::Debug, sync::Arc};
 
 use base64::prelude::*;
 use glamour::{Point2, Vector2};
+use ordered_float::OrderedFloat;
 use palette::Srgba;
-use parley::{swash::FontRef, Font as ParleyFont};
+use parley::{
+    swash,
+    swash::{FontRef, Tag},
+    Font as ParleyFont,
+};
 use serde::{Deserialize, Serialize};
 
 use crate::Resources;
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub struct Setting {
+    pub tag: Tag,
+    pub value: OrderedFloat<f32>,
+}
+
+impl From<&swash::Setting<f32>> for Setting {
+    fn from(value: &swash::Setting<f32>) -> Self {
+        Self {
+            tag: value.tag,
+            value: value.value.into(),
+        }
+    }
+}
+
+impl From<&Setting> for swash::Setting<f32> {
+    fn from(value: &Setting) -> Self {
+        Self {
+            tag: value.tag,
+            value: value.value.into(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub struct Synthesis {
+    pub vars: Vec<Setting>,
+    pub embolden: bool,
+    pub skew: OrderedFloat<f32>,
+}
+
+impl From<swash::Synthesis> for Synthesis {
+    fn from(value: swash::Synthesis) -> Self {
+        let vars = value.variations().iter().map(|v| v.into()).collect();
+        let embolden = value.embolden();
+        let skew = value.skew().unwrap_or(0.0).round().into();
+        Synthesis {
+            vars,
+            embolden,
+            skew,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct GlyphRun {
-    // TODO: Store synthesis paramaters from swash here
     pub position: Point2,
     pub font_id: FontId,
     pub font_index: usize,
     pub color: Srgba,
     pub size: f32,
     pub normalized_coords: Vec<i16>,
-
+    pub synthesis: Synthesis,
     pub glyphs: Vec<Glyph>,
 }
 
